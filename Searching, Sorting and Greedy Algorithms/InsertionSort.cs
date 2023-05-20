f25234d94683b8ff8cbb233de0d628b881fd2e11
Nikola Madzharov <nmadzharov1912@gmail.com>
2023-05-21 02:01:00 +0300

    Solved InsertionSort

---
+++ Searching, Sorting and Greedy Algorithms/InsertionSort.cs
@@ -0,0 +1,43 @@
+﻿
+
+using System.ComponentModel.Design;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+       var arr = Console.ReadLine().
+            Split().
+            Select(int.Parse)
+            .ToArray();
+
+       InsertionSort(arr);
+
+        Console.WriteLine(string.Join(" ", arr));
+
+
+
+    }
+
+    private static void InsertionSort(int[] arr)
+    {
+        for (int i = 1; i < arr.Length; i++)
+        {
+            var j = i;
+
+            while (j > 0 && arr[j - 1] > arr[j])
+            {
+                SwapElements(arr,j, j-1);
+                j -= 1;
+            }
+        }
+    }
+
+
+    private static void SwapElements(int[] arr, int first, int second)
+    {
+        var temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
