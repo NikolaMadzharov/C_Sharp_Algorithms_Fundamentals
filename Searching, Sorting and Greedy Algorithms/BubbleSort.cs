7e380961233f894aec51973c4b888d92c1d1754d
Nikola Madzharov <nmadzharov1912@gmail.com>
2023-05-21 01:34:25 +0300

    Solved BubbleSort

---
+++ Searching, Sorting and Greedy Algorithms/BubbleSort.cs
@@ -0,0 +1,51 @@
+﻿
+
+using System.ComponentModel.Design;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+       var arr = Console.ReadLine().
+            Split().
+            Select(int.Parse)
+            .ToArray();
+
+       BubbleSort(arr);
+
+        Console.WriteLine(string.Join(" ", arr));
+
+
+
+    }
+
+    private static void BubbleSort(int[] arr)
+    {
+
+        var isSorted = false;
+
+        while (!isSorted)
+        {
+            isSorted = true;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                var j = i - 1;
+
+                if (arr[j] > arr[i])
+                {
+                    SwapElements(arr,j,i);
+                    isSorted = false;
+                }
+            }
+        }
+    }
+
+
+    private static void SwapElements(int[] arr, int first, int second)
+    {
+        var temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
